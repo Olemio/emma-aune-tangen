00c00e1bee7001afbad1761a192d8557711ed1eb
Olemio <ailurus842@gmail.com>
2025-12-17 20:13:54 +0100

    init firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA5yZfnbGSyBQw9gYL1ziyJNMvtNZQS_eM",
+    authDomain: "emma-aune-tangen.firebaseapp.com",
+    projectId: "emma-aune-tangen",
+    storageBucket: "emma-aune-tangen.firebasestorage.app",
+    messagingSenderId: "872375024498",
+    appId: "1:872375024498:web:807ba9a31271a45aedbbeb",
+    measurementId: "G-LR5CTRS6JE",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
