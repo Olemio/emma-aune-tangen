2516aa1d78452fed821b5f2534b78972b86e80c7
Olemio <ailurus842@gmail.com>
2025-12-19 16:50:01 +0100

    mine bilder av mona lise

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
         <div className="page-wrapper">
             <Header />
 
-            <h1 className="text-lg">Mine Bilder</h1>
+            <h1 className="text-lg">Mine bilder av Mona Lise</h1>
 
             <div className="product-list">
                 {Array.from({ length: 10 }).map(() => (
