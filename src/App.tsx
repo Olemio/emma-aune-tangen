1e57940facc8f05db5cab4dcaba05be491118ed9
Olemio <ailurus842@gmail.com>
2025-12-19 17:08:40 +0100

    add 100 images of mona lise

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
             <h1 className="text-lg">Mine bilder av Mona Lise</h1>
 
             <div className="product-list">
-                {Array.from({ length: 10 }).map(() => (
+                {Array.from({ length: 100 }).map(() => (
                     <ProductCard img={monaLisa} name={"Mona Lise"} />
                 ))}
             </div>
