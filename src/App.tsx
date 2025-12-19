126294a8f28353f55cfbe9810049fa77db0aa2c9
Olemio <ailurus842@gmail.com>
2025-12-19 16:33:45 +0100

    add mona lise image

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,16 @@ function App() {
     return (
         <div className="page-wrapper">
             <Header />
+
+            <h1 className="text-lg">Mine Bilder</h1>
+
+            <div className="product-list">
+                <img
+                    className="image"
+                    src="../temp/mona-lisa-painting.jpg"
+                    alt=""
+                />
+            </div>
         </div>
     );
 }
