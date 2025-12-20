337a1dd6b380f974f71c47bb4cdc667c787be565
Olemio <ailurus842@gmail.com>
2025-12-20 06:24:55 +0100

    styling product cards

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,11 +6,11 @@ export default function Main() {
         <main className="flex flex-col items-center mx-auto w-full max-w-[1100px]">
             <h1 className="text-3xl mb-8">Mona Lisa Collection</h1>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 w-full">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 w-full">
                 {Array.from({ length: 100 }).map(() => (
                     <ProductCard
                         img={monaLisa}
-                        name={"Mona Lise"}
+                        name={'Orginal: "Mona Lise"'}
                         price={"99 992 NOK"}
                     />
                 ))}
