c6453bee6794bd29b9478ce52f0d8f51e3ea47c9
Olemio <ailurus842@gmail.com>
2025-12-20 06:22:50 +0100

    product cards spacing

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -10,7 +10,7 @@ export default function ProductCard({
     return (
         <div className="flex flex-col rounded-lg border border-gray-400">
             <img className="rounded-t" src={`${img}`} alt="" />
-            <div className="flex flex-col px-4 py-2 gap-1">
+            <div className="flex flex-col px-4 py-5 gap-1">
                 <p className="text-sm">{name}</p>
                 <p>{price}</p>
             </div>
