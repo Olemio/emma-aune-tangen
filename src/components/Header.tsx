a578046bb08f3cb8f318e0c45b1d233721d95648
Olemio <ailurus842@gmail.com>
2025-12-20 07:04:27 +0100

    move header text to left

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
 export default function Header() {
     return (
         <header className="flex flex-col mb-16 pt-4 mx-auto max-w-[1000px]">
-            <div className="flex justify-between items-center py-4  w-full">
+            <div className="flex flex-col py-4 w-full">
                 <p className="font-fustat text-2xl">Emma Aune-Tangen</p>
                 <p className="text-sm">emmaaunetangen@gmail.com</p>
             </div>
 
-            <div className="h-[1px] w-full  rounded-lg bg-gray-400"></div>
+            <div className="h-[1px] w-full rounded-lg bg-gray-400"></div>
         </header>
     );
 }
