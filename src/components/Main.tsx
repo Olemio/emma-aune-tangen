b24586c84c0d5d0eb3d5e3a270df740f33f747d8
Olemio <ailurus842@gmail.com>
2025-12-21 14:48:06 +0100

    change grid layout

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -47,7 +47,7 @@ export default function Main() {
                 />
             </div>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 w-full">
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 w-full">
                 {productItems.map(([path, src]) => {
                     const name = path
                         .split("/")
