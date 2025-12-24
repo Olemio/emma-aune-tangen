b777720bd3f7da472c806d0951c5690b76d4d28c
Oliver Bing <ailurus842@gmail.com>
2025-12-24 15:45:27 +0100

    layout for new login with google button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,18 @@
 export default function Header() {
     return (
-        <header className="flex flex-col mb-16 pt-4 mx-auto max-w-[1000px]">
-            <div className="flex flex-col justify-between items-center py-4 w-full">
-                <p className="font-fustat text-2xl">Emma Aune-Tangen</p>
-                <p className="text-sm">emmaaunetangen@gmail.com</p>
+        <header className="flex flex-col mb-16 pt-4 mx-auto max-w-250">
+            <div className="grid grid-cols-3">
+                <div></div>
+                <div className="flex flex-col justify-between items-center py-4 w-full">
+                    <p className="font-fustat text-2xl">Emma Aune-Tangen</p>
+                    <p className="text-sm">emmaaunetangen@gmail.com</p>
+                </div>
+                <div className="flex justify-end">
+                    <button>Login with Google</button>
+                </div>
             </div>
 
-            <div className="h-[1px] w-full rounded-lg bg-gray-400"></div>
+            <div className="h-px w-full rounded-lg bg-gray-400"></div>
         </header>
     );
 }
