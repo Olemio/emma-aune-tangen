c4a073e483684c3f76c9e429c5340e6282660f89
Olemio <ailurus842@gmail.com>
2026-01-03 18:15:20 +0100

    add cool git add

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -6,26 +6,47 @@ export default function Header() {
 
     React.useEffect(() => {
         const onScroll = () => {
-            setScrolled(window.scrollY > 10);
+            const y = window.scrollY;
+
+            setScrolled((prev) => {
+                if (!prev && y > 30) return true; // turn on after 30px
+                if (prev && y < 10) return false; // turn off before 10px
+                return prev;
+            });
         };
-        window.addEventListener("scroll", onScroll);
+
+        onScroll(); // init
+        window.addEventListener("scroll", onScroll, { passive: true });
         return () => window.removeEventListener("scroll", onScroll);
     }, []);
     return (
         <header
             className={`sticky top-0 z-48 transition-colors duration-300 flex flex-col mb-10 mx-auto max-w-[1400px] text-xl ${
-                scrolled
-                    ? "bg-amber-100/60 dark:bg-amber-900/40 backdrop-blur"
-                    : "bg-transparent"
+                scrolled ? "backdrop-blur-lg bg-slate-50/60" : "bg-transparent"
             }`}
         >
-            <div className="flex gap-8 justify-between items-center m-5">
+            <div
+                className={`
+            flex justify-between items-center
+            transition-all duration-300
+            ${scrolled ? "px-5 py-2" : "p-5"}
+        `}
+            >
                 <div>
                     <p className="font-title text-3xl dark:text-slate-100 text-slate-900">
                         Emma Aune-Tangen
                     </p>
                     <a
-                        className="text-slate-500 dark:text-slate-300"
+                        className={`
+    text-slate-500 dark:text-slate-300
+    block overflow-hidden
+    transition-[opacity,max-height,transform] duration-300
+    ${
+        scrolled
+            ? "max-h-0 opacity-0 -translate-y-1"
+            : "max-h-10 opacity-100 translate-y-0"
+    }
+  `}
                         href="mailto:emmaaunetangen@gmail.com"
                     >
                         emmaaunetangen@gmail.com
