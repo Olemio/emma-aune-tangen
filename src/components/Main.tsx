efa98c414c5340121720ae03b8474630741ddc92
Olemio <ailurus842@gmail.com>
2026-01-03 12:33:46 +0100

    remove junk from Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,22 +5,19 @@ import { artworks } from "../data/artworks.ts";
 
 export default function Main() {
     const [openModal, setOpenModal] = React.useState<string | undefined>();
+    const formatTitle = (title: string) => {
+        return title.split("/").pop()?.replace(".jpg", "").replaceAll("-", " ");
+    };
 
     return (
         <main className="mx-auto max-w-[1400px] px-5">
             <div className="columns-2 md:columns-3 lg:columns-4 gap-2.5">
                 {artworks.map(({ id, src, title }) => {
-                    const name = title
-                        .split("/")
-                        .pop()
-                        ?.replace(".jpg", "")
-                        .replaceAll("-", " ");
-
                     return (
                         <ProductCard
                             key={id}
                             img={src}
-                            name={name}
+                            name={formatTitle(title)}
                             price="12.000,00 NOK"
                             setOpenModal={setOpenModal}
                         />
