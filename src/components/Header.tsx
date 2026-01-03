a74c86d5237265366220bf20d9aee846159f91a9
Olemio <ailurus842@gmail.com>
2026-01-03 10:19:04 +0100

    add hamburger menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,9 @@
-import { Auth } from "./auth";
-import { NavLink } from "react-router-dom";
-import { ThemeToggle } from "./ThemeToggle.tsx";
+import Menu from "./Menu";
 
 export default function Header() {
     return (
         <header className="flex flex-col mb-10 mx-auto max-w-[1400px] text-xl">
-            <div className="flex flex-col md:flex-row gap-8 justify-between items-center m-5">
+            <div className="flex gap-8 justify-between items-center m-5">
                 <div>
                     <p className="font-title text-3xl dark:text-slate-100 text-slate-900">
                         Emma Aune-Tangen
@@ -14,26 +12,7 @@ export default function Header() {
                         emmaaunetangen@gmail.com
                     </p>
                 </div>
-                <div className="flex gap-10 dark:text-slate-200">
-                    <NavLink
-                        to="/"
-                        className={({ isActive }) =>
-                            isActive ? "text-amber-700" : ""
-                        }
-                    >
-                        PORTFOLIO
-                    </NavLink>
-                    <NavLink
-                        to="/about"
-                        className={({ isActive }) =>
-                            isActive ? "text-amber-700" : ""
-                        }
-                    >
-                        ABOUT
-                    </NavLink>
-                    <Auth />
-                    <ThemeToggle />
-                </div>
+                <Menu />
             </div>
         </header>
     );
---
+++ src/components/Menu.tsx
@@ -0,0 +1,69 @@
+import { NavLink } from "react-router-dom";
+import { Auth } from "./auth";
+import { ThemeToggle } from "./ThemeToggle";
+import React from "react";
+
+export default function Menu() {
+    const [isOpen, setIsOpen] = React.useState(false);
+    return (
+        <>
+            <div className="hidden md:flex gap-10 dark:text-slate-200">
+                <NavLink
+                    to="/"
+                    className={({ isActive }) =>
+                        isActive ? "text-amber-700" : ""
+                    }
+                >
+                    PORTFOLIO
+                </NavLink>
+                <NavLink
+                    to="/about"
+                    className={({ isActive }) =>
+                        isActive ? "text-amber-700" : ""
+                    }
+                >
+                    ABOUT
+                </NavLink>
+                <Auth />
+                <ThemeToggle />
+            </div>
+
+            <button onClick={() => setIsOpen(true)} className="md:hidden flex">
+                ☰
+            </button>
+            {isOpen ? (
+                <div className="fixed flex flex-col items-center bg-amber-200/95 inset-0 z-50">
+                    <div className="flex justify-between p-8 w-full">
+                        <div className="flex items-center gap-8">
+                            <Auth />
+                            <ThemeToggle />
+                        </div>
+                        <button onClick={() => setIsOpen(false)}>Close</button>
+                    </div>
+                    <div className="flex flex-col items-center h-full w-full gap-20 mt-20">
+                        <NavLink
+                            to="/"
+                            className={({ isActive }) =>
+                                isActive
+                                    ? "text-amber-700 text-3xl md:text-xl"
+                                    : "text-3xl md:text-xl"
+                            }
+                        >
+                            PORTFOLIO
+                        </NavLink>
+                        <NavLink
+                            to="/about"
+                            className={({ isActive }) =>
+                                isActive
+                                    ? "text-amber-700 text-3xl md:text-xl"
+                                    : "text-3xl md:text-xl"
+                            }
+                        >
+                            ABOUT
+                        </NavLink>
+                    </div>
+                </div>
+            ) : null}
+        </>
+    );
+}
