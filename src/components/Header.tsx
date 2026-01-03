bf503e02810399459fc907f89c4fe691f61914e4
Olemio <ailurus842@gmail.com>
2026-01-03 09:27:30 +0100

    add some mobile support

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import { ThemeToggle } from "./ThemeToggle.tsx";
 export default function Header() {
     return (
         <header className="flex flex-col mb-10 mx-auto max-w-[1400px] text-xl">
-            <div className="flex justify-between items-center m-5">
+            <div className="flex flex-col md:flex-row gap-8 justify-between items-center m-5">
                 <div>
                     <p className="font-title text-3xl dark:text-slate-100 text-slate-900">
                         Emma Aune-Tangen
