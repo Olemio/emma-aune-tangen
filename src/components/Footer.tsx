b7d8ccf49ec70e93ef605d349e93229922d5b1e1
Olemio <ailurus842@gmail.com>
2026-01-03 11:18:37 +0100

    add stuff to header

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,10 @@
 export default function Footer() {
     return (
         <footer className="flex flex-col items-center mt-10 py-40 text-slate-500 bg-amber-100 dark:text-slate-400 dark:bg-amber-900/60">
-            <p>emmaaunetangen@gmail.com</p>
+            <p>Emma Aune-Tangen</p>
+            <a href="mailto:emmaaunetangen@gmail.com">
+                emmaaunetangen@gmail.com
+            </a>
         </footer>
     );
 }
