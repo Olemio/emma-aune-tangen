99cee35eb522d6812160e410f7ed6c1873d09650
Olemio <ailurus842@gmail.com>
2026-01-03 01:24:08 +0100

    change product card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -10,12 +10,9 @@ export default function ProductCard({
     setOpenModal: (img: string | undefined) => void;
 }) {
     return (
-        <button
-            onClick={() => setOpenModal(img)}
-            className="flex flex-col rounded-lg"
-        >
-            <img className="rounded object-cover" src={`${img}`} alt="" />
-            <div className="flex flex-col items-start px-4 py-5 gap-1">
+        <button onClick={() => setOpenModal(img)} className="flex flex-col">
+            <img className="object-cover" src={`${img}`} alt="" />
+            <div className="flex flex justify-between py-2 gap-1">
                 <p className="text-sm">{name}</p>
                 <p>{price}</p>
             </div>
