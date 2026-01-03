bc691967b13ebcd2bc10b121a5176702348a58a0
Olemio <ailurus842@gmail.com>
2026-01-03 11:12:08 +0100

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import About from "./components/About.tsx";
+import Footer from "./components/Footer.tsx";
 
 export default function App() {
     return (
@@ -12,6 +13,7 @@ export default function App() {
                 <Route path="/about" element={<About />} />
                 <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
+            <Footer />
         </>
     );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="flex flex-col items-center mt-10 py-40 text-slate-500 bg-amber-100 dark:text-slate-400 dark:bg-amber-900/60">
+            <p>emmaaunetangen@gmail.com</p>
+        </footer>
+    );
+}
