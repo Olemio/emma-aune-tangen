20e4e5b10ede127d90edd0ade2969654c7bfecac
Olemio <ailurus842@gmail.com>
2026-01-03 05:20:16 +0100

    product card styling

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -10,9 +10,27 @@ export default function ProductCard({
     setOpenModal: (img: string | undefined) => void;
 }) {
     return (
-        <button onClick={() => setOpenModal(img)} className="flex flex-col">
-            <img className="" src={`${img}`} alt="" />
-            <div className="flex px-2 pt-1 pb-4">
+        <button
+            onClick={() => setOpenModal(img)}
+            className="group flex flex-col cursor-pointer"
+        >
+            <div className="relative">
+                <img
+                    src={img}
+                    alt={name}
+                    className="w-full transition-opacity duration-300 group-hover:opacity-80"
+                />
+
+                <div className="absolute inset-0 bg-amber-700/0 duration-300 group-hover:bg-amber-700/50" />
+
+                <div className="absolute inset-0 flex items-center justify-center">
+                    <span className="font-title font-semibold italic text-xl tracking-[0.2em] text-slate-50 opacity-0 translate-y-1 transition-all duration-100 group-hover:opacity-100 group-hover:translate-y-0">
+                        {name}
+                    </span>
+                </div>
+            </div>
+
+            <div className="flex px-2 pt-1 pb-6">
                 <p className="text-sm text-slate-500">{price}</p>
             </div>
         </button>
