0a16ddb81e054fa6545cc878d0cbe8bd4b2a6ca9
Olemio <ailurus842@gmail.com>
2026-01-03 01:21:39 +0100

    use a masonry container? What

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -50,7 +50,7 @@ export default function Main() {
                 />
             </div>
 
-            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 w-full">
+            <div className="columns-2 md:columns-3 gap-6 w-full">
                 {productItems.map(([path, src]) => {
                     const name = path
                         .split("/")
