d2c9b28b81fd29d248a948552c492e19b2089df8
Olemio <ailurus842@gmail.com>
2026-01-03 18:15:26 +0100

    cool stuff

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -9,28 +9,28 @@ export default function Header() {
             const y = window.scrollY;
 
             setScrolled((prev) => {
-                if (!prev && y > 30) return true; // turn on after 30px
-                if (prev && y < 10) return false; // turn off before 10px
+                if (!prev && y > 30) return true;
+                if (prev && y < 10) return false;
                 return prev;
             });
         };
 
-        onScroll(); // init
+        onScroll();
         window.addEventListener("scroll", onScroll, { passive: true });
         return () => window.removeEventListener("scroll", onScroll);
     }, []);
     return (
         <header
-            className={`sticky top-0 z-48 transition-colors duration-300 flex flex-col mb-10 mx-auto max-w-[1400px] text-xl ${
+            className={`sticky top-0 z-48 transition-colors duration-300 flex flex-col mb-10    mx-auto max-w-[1400px] text-xl ${
                 scrolled ? "backdrop-blur-lg bg-slate-50/60" : "bg-transparent"
             }`}
         >
             <div
                 className={`
-            flex justify-between items-center
-            transition-all duration-300
-            ${scrolled ? "px-5 py-2" : "p-5"}
-        `}
+                    flex justify-between items-center
+                    transition-all duration-300
+                    ${scrolled ? "px-5 py-2" : "p-5"}
+                `}
             >
                 <div>
                     <p className="font-title text-3xl dark:text-slate-100 text-slate-900">
@@ -38,15 +38,15 @@ export default function Header() {
                     </p>
                     <a
                         className={`
-    text-slate-500 dark:text-slate-300
-    block overflow-hidden
-    transition-[opacity,max-height,transform] duration-300
-    ${
-        scrolled
-            ? "max-h-0 opacity-0 -translate-y-1"
-            : "max-h-10 opacity-100 translate-y-0"
-    }
-  `}
+                            text-slate-500 dark:text-slate-300
+                            block overflow-hidden
+                            transition-[opacity,max-height,transform] duration-300
+                            ${
+                                scrolled
+                                    ? "max-h-0 opacity-0 -translate-y-1"
+                                    : "max-h-10 opacity-100 translate-y-0"
+                            }
+                        `}
                         href="mailto:emmaaunetangen@gmail.com"
                     >
                         emmaaunetangen@gmail.com
