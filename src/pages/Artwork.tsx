0cd3e0667415723e72dea2cd20574bb90b678852
Oliver Mio Leander Bing-Hansen <ailurus842@gmail.com>
2026-01-15 23:26:08 +0100

    move artwork page title to top of image

--- src/pages/Artwork.tsx
+++ src/pages/Artwork.tsx
@@ -17,6 +17,8 @@ export default function Artwork() {
   
   return (
     <div className="max-w-[1400px] mx-auto p-6 flex flex-col md:flex-row gap-8">
+
+        <h1 className="text-center text-3xl font-title italic font-bold">{art.title}</h1>
       <img
         className="w-full md:w-2/3 max-h-[80vh] object-contain"
         src={art.src}
@@ -24,7 +26,6 @@ export default function Artwork() {
       />
 
       <div className="md:w-1/3 flex flex-col gap-4">
-        <h1 className="text-3xl font-title italic font-bold">{art.title}</h1>
         <p>{art.description}</p>
 
         {(art.price || art.sold || art.year || art.size) && (
